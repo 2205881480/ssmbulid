9c888778167f8f0fa635a41a70a061e992f31deb
zyh <2205881480@qq.com>
2022-10-17 17:10:44 +0800

    ssm

---
+++ src/main/java/com/kuang/controller/BookController.java
@@ -0,0 +1,83 @@
+package com.kuang.controller;
+
+import com.kuang.pojo.Books;
+import com.kuang.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    //Controller 调service层
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    //查询全部的书籍，并且返回到一个书籍展示页面
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+    //添加书籍
+    @RequestMapping("/toAddBook")
+    public String toAddPaper() {
+        return "addBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        return "redirect:/book/allBook";
+    }
+    //修改书籍
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook( int id,Model model) {
+        Books books = bookService.queryBookById(id);
+        System.out.println(books);
+        model.addAttribute("book",books );
+        return "updateBook";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(Model model, Books book) {
+        System.out.println(book);
+        bookService.updateBook(book);
+        Books books = bookService.queryBookById(book.getBookID());
+        model.addAttribute("books", books);
+        return "redirect:/book/allBook";
+    }
+    //删除书籍
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+
+    //查询书籍
+    @RequestMapping("/queryBook")
+    public String queryBook(String queryBookName,Model model){
+        Books books = bookService.queryBookByName(queryBookName);
+        System.out.println(books);
+        List<Books> list=new ArrayList<>();
+        list.add(books);
+
+        if(books==null){
+            list=bookService.queryAllBook();
+            model.addAttribute("error","未查到");
+        }
+        model.addAttribute("list", list);
+        return "allBook";
+
+    }
+}
